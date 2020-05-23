c074d950893072d5a332f406d1f12dc7355bf593
Tiago José <tiago_jvo@outlook.com>
2020-05-23 10:56:52 -0300

    Code refactoring , adding APIFeatures utils

---
+++ utils/ApiFeatures.js
@@ -0,0 +1,65 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach((field) => {
+      delete queryObj[field];
+    });
+    //2B - Advanced filtering
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    queryString = JSON.parse(queryString);
+
+    this.query = this.query.find(queryString);
+
+    return this;
+    // const query = Tour.find(queryString);
+  }
+
+  sort() {
+    if (this.query.sort) {
+      const sortBy = this.query.sort.split(',').join(' ');
+
+      this.query = this.query.sort(sortBy);
+
+      //sort('price ratingsAverage')
+    } else {
+      this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.req.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+      //select('name price')
+    } else {
+      this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  pagination() {
+    const page = parseInt(this.queryString.page, 10) || 1;
+    const limit = parseInt(this.queryString.limit, 10) || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
